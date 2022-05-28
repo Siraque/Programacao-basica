f0522bd38724aa63c3dc56a8dd0e45bc7df66d9d
Mateus Siraque <msiraque@hotmail.com>
2022-05-27 22:58:53 -0300

    Calculadora e teste xunit

---
+++ xunitCalculator.Test/Calculator_Test.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xunitCalculartor;
+using Xunit;
+
+namespace xunitCalculator.Test
+{
+    public class Calculator_Test
+    {
+        [Fact]
+        public void Add_CalculatorShouldAddTwoNumbers()
+        {
+            //Arrange
+            double number1 = 5.5;
+            double number2 = 4.5;
+            double expectedResult = 10;
+
+            //Act
+            double actualResult = Calculator.Addition(number1, number2);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void Sub_CalculatorShouldSubTwoNumbers()
+        {
+            //Arrange
+            double number1 = 4.2;
+            double number2 = 5.2;
+            double expectResult = -1;
+
+            //Act
+            double actualResult = Calculator.subtraction(number1, number2);
+
+            //Assert
+            Assert.Equal(expectResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 6)]
+        [InlineData(5.25, 98.4, 516.6)]
+        [InlineData(-5, 5, -25)]
+        public void Mult_CalculatorShouldMultTwoNumbers(double a, double b, double expectedResult)
+        {
+            //Arrange
+
+            //Act
+            double actualResult = Calculator.Multiplication(a, b);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+
+        [Theory]
+        [InlineData(10, 5, 2)]
+        [InlineData(0, 2, 0)]
+        [InlineData(-20, -10, 2)]
+        [InlineData(-5, 5, -1)]
+        public void Div_CalculatorShouldDivTwoNumbers(double a, double b, double expectedResult)
+        {
+            //Arrange
+
+            //Act
+            double actualResult = Calculator.Division(a, b);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Theory]
+        [InlineData(1, 0, "Divide by Zero Error!")]
+        [InlineData(0, 0, "Divide by Zero Error!")]
+        public void Div_ZeroException(double a, double b, string expectedErrorMessage)
+        {
+            //Arrange
+
+            //Act
+            var ex = Assert.Throws<DivideByZeroException>(() => Calculator.Division(a, b));
+
+            //Assert
+            Assert.Equal(expectedErrorMessage, ex.Message);
+        }
+
+
+    }
+}
---
+++ xunitTest/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xunitCalculartor
+{
+    public static class Calculator
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Divide by Zero Error!");
+            return a / b;
+        }
+
+    }
+}
